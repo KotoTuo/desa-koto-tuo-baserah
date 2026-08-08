929986f2ef94fe61124793bcccb915c146c8d78a
venus <shandaazkiya@gmail.com>
2026-08-08 14:55:19 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const menuIcon = menuToggle.querySelector('i');
 // Toggle menu
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
+    menuToggle.classList.toggle('active');
     // Ganti ikon
     if (navMenu.classList.contains('active')) {
         menuIcon.classList.remove('fa-bars');
@@ -17,20 +18,38 @@ menuToggle.addEventListener('click', () => {
 document.querySelectorAll('#nav-menu a').forEach(link => {
     link.addEventListener('click', () => {
         navMenu.classList.remove('active');
+        menuToggle.classList.remove('active');
         menuIcon.classList.remove('fa-xmark');
         menuIcon.classList.add('fa-bars');
     });
 });
 const overlay = document.querySelector(".menu-overlay");
-
 menuToggle.addEventListener("click", () => {
     overlay.classList.toggle("active");
 });
-
 overlay.addEventListener("click", () => {
     navMenu.classList.remove("active");
     overlay.classList.remove("active");
-
+    menuToggle.classList.remove("active");
     menuIcon.classList.remove("fa-xmark");
     menuIcon.classList.add("fa-bars");
+});
+// ================= LIGHTBOX GALERI =================
+const galleryImages = document.querySelectorAll('.gambar img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const lightboxClose = document.querySelector('.lightbox-close');
+galleryImages.forEach(image => {
+    image.addEventListener('click', () => {
+        lightbox.classList.add('active');
+        lightboxImg.src = image.src;
+    });
+});
+lightboxClose.addEventListener('click', () => {
+    lightbox.classList.remove('active');
+});
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+        lightbox.classList.remove('active');
+    }
 });
